895caf72fb67a92aebd43b7efacdec6fad5e686c
Fukuyama Keiske <verve@is-gode.ru>
2024-12-07 16:01:06 +0300

    Back-End | Dropped PayToUse yandex API, moved to 2GIS | V1.5

--- backend/app/services/traffic_analysis.py
+++ backend/app/services/traffic_analysis.py
@@ -4,17 +4,17 @@ from app.core.config import settings
 
 async def get_traffic_data(waypoints: list):
     # Яндекс Карт
-    yandex_url = "https://api.routing.yandex.net/v2/route"
-    yandex_params = {
-        "apikey": settings.YANDEX_MAPS_API_KEY,
-        "waypoints": "|".join(waypoints),
-        "mode": "driving",
-        "traffic": "realtime"
-    }
+    # yandex_url = "https://api.routing.yandex.net/v2/route"
+    # yandex_params = {
+    #     "apikey": settings.YANDEX_MAPS_API_KEY,
+    #     "waypoints": "|".join(waypoints),
+    #     "mode": "driving",
+    #     "traffic": "realtime"
+    # }
     
-    async with httpx.AsyncClient() as client:
-        yandex_response = await client.get(yandex_url, params=yandex_params)
-        yandex_data = yandex_response.json()
+    # async with httpx.AsyncClient() as client:
+    #     yandex_response = await client.get(yandex_url, params=yandex_params)
+    #     yandex_data = yandex_response.json()
 
     # 2ГИС
     twogis_url = "http://routing.api.2gis.com/routing/7.0.0/global"
@@ -29,4 +29,4 @@ async def get_traffic_data(waypoints: list):
         twogis_response = await client.post(twogis_url, json=twogis_payload)
         twogis_data = twogis_response.json()
 
-    return {"yandex": yandex_data, "twogis": twogis_data}
+    return {"twogis": twogis_data}
